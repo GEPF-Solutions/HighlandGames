374f113ff8305c7f9668a67cc695a9c1795c01ef
Lukas Pfanner <lukas.pfann@gmail.com>
2026-05-03 21:09:42 +0200

    Corrected discipline names

---
+++ HighlandGames.Server/Migrations/20260503185359_03-05-2026.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HighlandGames.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class _03052026 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: "fire",
+                column: "Name",
+                value: "Durstlöschen");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Disciplines",
+                keyColumn: "Id",
+                keyValue: "fire",
+                column: "Name",
+                value: "Dustlöschen");
+        }
+    }
+}
